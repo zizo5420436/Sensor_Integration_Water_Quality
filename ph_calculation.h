124134fd0ec2519aae1e69e48c13f2eafcde08de
MUHAMMED SANIN A <160201215+zizo5420436@users.noreply.github.com>
2026-01-15 18:39:09 +0530

    Update ph_calculation.h

--- ph_calculation.h
+++ ph_calculation.h
@@ -9,7 +9,13 @@
 #define INC_PH_CALCULATION_H_
 
 #include <stdint.h>
+#define PH1_VALUE   4.00f
+#define PH2_VALUE   6.86f
+#define PH3_VALUE   9.18f
 
+#define V1_PH4      1.770f
+#define V2_PH686    1.639f
+#define V3_PH918    1.496f
 float Calculate_pH_2Point(float V, float V1, float pH1,
                           float V2, float pH2);
 
@@ -21,3 +27,4 @@ float Calculate_pH_3Point(float V,
 float voltage_for_ph(uint16_t val);
 void ph_France_Runnnig_temp(void);
 #endif /* INC_PH_CALCULATION_H_ */
+
